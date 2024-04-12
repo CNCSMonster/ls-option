6acea4b7a442bec22853c6a5bb540cc190d190a4
cncsmonster <cncsmonster@163.com>
2024-04-13 01:51:47 +0800

    add docs

--- README.md
+++ README.md
@@ -3,7 +3,31 @@
 ## Quick Start
 
 ```zsh
+# try to list all files with suffix `.rs` in current path
 cargo run --example list_all_rs
+# try to list paths in an unexisted path
+cargo run --example list_unexisted_path
+```
+
+Example 1：
+```rust
+    use ls_option::*;
+    let fs = ListOption::default()
+        // allow to show file
+        .file(true)
+        // not allow to show dir
+        .dir(false)
+        // allow to show unhidden
+        .unhidden(true)
+        // not allow to show hidden
+        .hidden(false)
+        // walk recursively to search path
+        .recursive(true)
+        // only show path with one of these suffix
+        .sufs(vec![".rs"])
+        // start list the expected path search from `.`
+        .list(".");
+    dbg!(fs);
 ```
 
 ## License
--- src/option.rs
+++ src/option.rs
@@ -17,6 +17,7 @@ pub struct ListOption {
     // if not empty, list only files with these extensions
     sufs: Vec<String>,
 }
+// Default implementation for ListOption
 impl Default for ListOption {
     fn default() -> Self {
         Self {
@@ -33,42 +34,87 @@ impl Default for ListOption {
 
 /// specify the list option
 impl ListOption {
+    /// set if allow this option to show directories
     pub fn dir(&mut self, if_show: bool) -> Self {
         self.dir = if_show;
         self.clone()
     }
+
+    /// set if allow this option to show files
     pub fn file(&mut self, if_show: bool) -> Self {
         self.file = if_show;
         self.clone()
     }
+
+    /// set if allow this option to show hidden files
     pub fn hidden(&mut self, if_show: bool) -> Self {
         self.hidden = if_show;
         self.clone()
     }
+
+    /// set if allow this option to show unhidden files
     pub fn unhidden(&mut self, if_show: bool) -> Self {
         self.unhidden = if_show;
         self.clone()
     }
+
+    /// set the level of recursion while listing files in some path
     pub fn level(&mut self, level: usize) -> Self {
         self.level = level;
         self.clone()
     }
+
+    /// set if allow this option to list recursively
     pub fn recursive(&mut self, if_choose: bool) -> Self {
         self.recursive = if_choose;
         self.clone()
     }
+
+    /// add one ext to the list of allowed extensions
+    /// only files with one of these extensions will be listed
+    /// e.g. ext("rs") will allow files with .rs extension to be listed
+    /// e.g. ext("rs").ext("toml") will allow files with .rs and .toml extensions to be listed
     pub fn ext(&mut self, ext: &str) -> Self {
         self.sufs.push(format!(".{}", ext));
         self.clone()
     }
+
+    /// add multiple exts to the list of allowed extensions
+    /// only files with one of these extensions will be listed
+    /// but this function will shadow the previous sufs
+    /// e.g. exts(vec!["rs", "toml"]) will allow files with .rs and .toml extensions to be listed
+    /// e.g. exts(vec!["rs"]).exts(vec!["toml"]) will only allow files with .toml extensions to be listed
     pub fn exts(&mut self, exts: Vec<&str>) -> Self {
         self.sufs = exts.iter().map(|s| format!(".{}", s)).collect();
         self.clone()
     }
+
+    /// add one suf to the list of allowed suffixes
+    /// only files with one of these suffixes will be listed
+    /// e.g. suf("rs") will allow files with rs suffix to be listed
+    /// notice that exts function will shadow the previous sufs
+    /// e.g. suf(".rs").ext("toml") will only allow files with .rs and .toml extensions to be listed
+    /// e.g. suf(".rs").suf(".toml") will only allow files with .toml extensions to be listed
+    /// e.g. suf("rs").exts(vec!["toml"]) will only allow files with .toml extensions to be listed
     pub fn suf(&mut self, suf: &str) -> Self {
         self.sufs.push(suf.to_string());
         self.clone()
     }
+
+    /// add multiple sufs to the list of allowed suffixes
+    ///
+    /// only files with one of these suffixes will be listed
+    ///
+    /// but this function will shadow the previous sufs
+    ///
+    /// e.g. sufs(vec!["rs", "toml"]) will allow files with rs and toml suffixes to be listed
+    ///
+    /// e.g. sufs(vec![".rs"]).sufs(vec![".toml"]) will only allow files with .toml extensions to be listed
+    ///
+    /// e.g. sufs(vec![".rs"]).ext("toml") will allow files with .rs or .toml extension to be listed
+    ///
+    /// e.g. sufs(vec!["rs"]).exts(vec!["toml"]) will only allow files with .toml extensions to be listed
+    ///
     pub fn sufs(&mut self, sufs: Vec<&str>) -> Self {
         self.sufs = sufs.iter().map(|s| s.to_string()).collect();
         self.clone()
@@ -77,27 +123,11 @@ impl ListOption {
 
 /// impl list functionality
 impl ListOption {
-    fn check_if_show_file(&self, file_path: &str) -> bool {
-        let base_name = Path::new(file_path).file_name().unwrap().to_str().unwrap();
-        // dbg!(file_path);
-        // dbg!(self.sufs.is_empty() || self.sufs.iter().any(|suf| base_name.ends_with(suf)));
-        // dbg!(&self.sufs);
-        self.file
-            && (self.sufs.is_empty() || self.sufs.iter().any(|suf| base_name.ends_with(suf)))
-            && (self.hidden && base_name.starts_with('.')
-                || self.unhidden && !base_name.starts_with('.'))
-    }
-    fn check_if_list_dir(&self, dir_path: &str) -> bool {
-        let dir_path = Path::new(dir_path).canonicalize().unwrap();
-        let base_name = dir_path
-            .file_name()
-            .unwrap_or_else(|| panic!("{:?}", dir_path))
-            .to_str()
-            .unwrap();
-        (self.level > 0 || self.recursive)
-            && (self.hidden && base_name.starts_with('.')
-                || self.unhidden && !base_name.starts_with('.'))
-    }
+    /// Lists the files and directories at the given path according to the options set in the ListOption
+    ///
+    /// if the path is a file, it will be listed if it matches the options set in the ListOption
+    ///
+    /// if the path is a directory, all files and directories in it will be listed if they match the options set in the ListOption
     pub fn list(&self, path: &str) -> Vec<String> {
         // check if is a file
         let path = Path::new(path);
@@ -144,6 +174,31 @@ impl ListOption {
         }
         ret
     }
+    // Checks if a file should be shown based on the options set in the ListOption
+    fn check_if_show_file(&self, file_path: &str) -> bool {
+        let base_name = Path::new(file_path).file_name().unwrap().to_str().unwrap();
+        // dbg!(file_path);
+        // dbg!(self.sufs.is_empty() || self.sufs.iter().any(|suf| base_name.ends_with(suf)));
+        // dbg!(&self.sufs);
+        self.file
+            && (self.sufs.is_empty() || self.sufs.iter().any(|suf| base_name.ends_with(suf)))
+            && (self.hidden && base_name.starts_with('.')
+                || self.unhidden && !base_name.starts_with('.'))
+    }
+    // Checks if a directory should be listed based on the options set in the ListOption
+    fn check_if_list_dir(&self, dir_path: &str) -> bool {
+        let dir_path = Path::new(dir_path).canonicalize().unwrap();
+        let base_name = dir_path
+            .file_name()
+            .unwrap_or_else(|| panic!("{:?}", dir_path))
+            .to_str()
+            .unwrap();
+        (self.level > 0 || self.recursive)
+            && (self.hidden && base_name.starts_with('.')
+                || self.unhidden && !base_name.starts_with('.'))
+    }
+
+    // Lists the files and directories at the given path according to the options set in the ListOption, used for recursive listing
     fn inner_list(&self, path: &str) -> Vec<String> {
         // check if is a file
         let path = Path::new(path);
